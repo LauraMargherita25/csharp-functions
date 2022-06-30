b5aab5309a737609a724a9a362323cf1f7548776
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-06-30 21:02:36 +0200

    Code cleaning

--- Program.cs
+++ Program.cs
@@ -1,58 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]“. Potete prendere quella fatta in classe questa mattina
+int[] intArray = {2, 6, 7, 5, 3, 9};
+int[] arreyAlQuadrato = ElevaArrayAlQuadrato(intArray);
 
-int[]  intArray = {1, 2, 3};
-
+// void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array
 void StampaArray(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine(array[i]);
+        Console.Write(array[i] + " ");
     }
+
+    Console.WriteLine();
 }
 
-StampaArray(intArray);
-
 // int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
-
 int Quadrato(int numero)
 {
     int result = numero * numero;
     return result;
 };
 
-Console.WriteLine(Quadrato(4));
-
-
-//int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
-
+//int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! 
 int[] ElevaArrayAlQuadrato(int[] array)
 {
-    int[] clonedArray = (int[]) array.Clone();
+    int[] clonedArray = (int[])array.Clone(); // senza conversione esplicita array.Clone() torna un oggetto
 
     for (int i = 0; i < clonedArray.Length; i++)
     {
-        Console.WriteLine(Quadrato(clonedArray[i]));
+        clonedArray[i] = Quadrato(clonedArray[i]);
     }
+
     return clonedArray;
 }
 
-ElevaArrayAlQuadrato(intArray);
-
 //int sommaElementiArray(int[] array): che preso un array di numeri interi, restituisca la somma totale di tutti gli elementi dell’array.
-
-int sum = 0;
 int sommaElementiArray(int[] array)
 {
+    int sum = 0;
     for (int i = 0; i < array.Length; i++)
     {
         sum += array[i];
     }
-    Console.WriteLine(sum);
     return sum;
 }
 
-sommaElementiArray(intArray);
 
+Console.Write("Questi sono i tuoi numeri: ");
+StampaArray(intArray);
 
+Console.Write("I tuoi numeri elevati al quadrato: ");
+StampaArray(arreyAlQuadrato);
+
+Console.WriteLine("La somma dei tuoi numeri: {0}",sommaElementiArray(intArray));
+
+Console.WriteLine("La somma dei tuoi numeri elevati al quadrato: {0}", sommaElementiArray(arreyAlQuadrato));
